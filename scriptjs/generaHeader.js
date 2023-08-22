fb2c1365c6c31cc1fbfff145b552cc987faecee8
FrankLDF <frankluisduarte00@gmail.com>
2023-08-21 23:56:58 -0400

    agregando codigo inicial y algunos estilos del blog

---
+++ scriptjs/generaHeader.js
@@ -0,0 +1,8 @@
+document.open();
+document.write(
+  '<header><a class="box-container  quitar-linea" href="index.html"><div class="logo_container"><img src="multimedia/FDFdeveloperLOGOpng.png" alt=""></div>  </a><div class="box-container"><a href="conoceme.html">Conóceme</a><a href="experiencia.html">Experiencia</a><a href="contactos.html">Contactos</a></div></header>'
+
+  // <div class="nombre_logo"><p>FrankLDF</p></div>
+);
+document.close();
+
